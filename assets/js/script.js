23fe23b2707af5edc9d2baa3ae7bcef4b69a528c
sujonmahamud05 <sujonmahamud05@gmail.com>
2023-01-08 12:24:00 +0600

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,16 +62,6 @@ $(document).ready(function () {
   // <!-- emailjs to mail contact form data -->
 });
 
-document.addEventListener("visibilitychange", function () {
-  if (document.visibilityState === "visible") {
-    document.title = "Portfolio | Jigar Sable";
-    $("#favicon").attr("href", "assets/images/favicon.png");
-  } else {
-    document.title = "Come Back To Portfolio";
-    $("#favicon").attr("href", "assets/images/favhand.png");
-  }
-});
-
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
   strings: [
@@ -87,82 +77,6 @@ var typed = new Typed(".typing-text", {
   backDelay: 500,
 });
 // <!-- typed js effect ends -->
-
-async function fetchData(type = "skills") {
-  let response;
-  type === "skills"
-    ? (response = await fetch("skills.json"))
-    : (response = await fetch("./projects/projects.json"));
-  const data = await response.json();
-  return data;
-}
-
-function showSkills(skills) {
-  let skillsContainer = document.getElementById("skillsContainer");
-  let skillHTML = "";
-  skills.forEach((skill) => {
-    skillHTML += `
-        <div class="bar">
-              <div class="info">
-                <img src=${skill.icon} alt="skill" />
-                <span>${skill.name}</span>
-              </div>
-            </div>`;
-  });
-  skillsContainer.innerHTML = skillHTML;
-}
-
-function showProjects(projects) {
-  let projectsContainer = document.querySelector("#work .box-container");
-  let projectHTML = "";
-  projects
-    .slice(0, 10)
-    .filter((project) => project.category != "android")
-    .forEach((project) => {
-      projectHTML += `
-        <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>`;
-    });
-  projectsContainer.innerHTML = projectHTML;
-
-  // <!-- tilt js effect starts -->
-  VanillaTilt.init(document.querySelectorAll(".tilt"), {
-    max: 15,
-  });
-  // <!-- tilt js effect ends -->
-
-  /* ===== SCROLL REVEAL ANIMATION ===== */
-  const srtop = ScrollReveal({
-    origin: "top",
-    distance: "80px",
-    duration: 1000,
-    reset: true,
-  });
-
-  /* SCROLL PROJECTS */
-  srtop.reveal(".work .box", { interval: 200 });
-}
-
-fetchData().then((data) => {
-  showSkills(data);
-});
-
-fetchData("projects").then((data) => {
-  showProjects(data);
-});
 
 // <!-- tilt js effect starts -->
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
